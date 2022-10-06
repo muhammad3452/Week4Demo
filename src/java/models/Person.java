cf6af3f315343a3283b39bc279a2c5f52cf3e4b1
muhammad3452 <113306223+muhammad3452@users.noreply.github.com>
2022-10-06 11:40:00 -0600

    Added Person bean

---
+++ src/java/models/Person.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author muham
+ */
+public class Person implements Serializable {
+   private String firstname;
+   private String lastname;
+
+    public Person() {
+        firstname = "";
+        lastname = "";
+    }
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+   
+}
